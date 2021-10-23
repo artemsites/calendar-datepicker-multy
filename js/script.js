716f6448ce2ce84605cf1545b5f8eddddffe5095
Artem Kuznecov <work.artem.kuznecov.samara@yandex.ru>
2021-10-24 02:35:30 +0400

    init

---
+++ js/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  
+  var datepickerCircleTricks = document.querySelector('.datepicker-circle-tricks');
+  var instance = M.Datepicker.init(datepickerCircleTricks);
+
+
+
+  datepickerCircleTricks.addEventListener('click', function () {
+    if (instance.isOpen) {
+      var dataDayList = document.querySelectorAll('[data-day]');
+      var cursorActive = false;
+
+
+
+      document.addEventListener('mousedown', function () {
+        cursorActive = true;
+        console.log('cursorActive = true;');
+      });
+
+      document.addEventListener('mouseup', function () {
+        cursorActive = false;
+        console.log('cursorActive = false;');
+      });
+
+
+
+      dataDayList.forEach(function (day) {
+        day.addEventListener('mouseover', function () {
+          console.log('mouseover');
+          if (cursorActive) {
+            console.log('cursorActive');
+            day.classList.add('is-selected');
+            day.setAttribute('aria-selected', 'true');
+          }
+          if (!cursorActive) {
+            console.log('!cursorActive');
+          }
+        });
+      });
+
+
+
+    }
+  });
+
+
+
+});
