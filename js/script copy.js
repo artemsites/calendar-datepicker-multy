411122b69b0edc33af8f98f22b179e0554140153
Artem <artem.kuznecov.samara@yandex.ru>
2022-08-09 11:24:53 +0400

    upd

--- js/script copy.js
+++ js/script copy.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
-
+  
   var datepickerCircleTricks = document.querySelector('.calendar-datepicker-multiple');
   var instance = M.Datepicker.init(datepickerCircleTricks);
 
@@ -27,20 +27,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
       dataDayList.forEach(function (day) {
-        day.addEventListener('mousedown', function () {
-          cursorActive = true;
-          console.log('cursorActive = true;');
-          day.addEventListener('mouseover', function () {
-            console.log('mouseover');
+        day.addEventListener('mouseover', function () {
+          console.log('mouseover');
+          if (cursorActive) {
             console.log('cursorActive');
             day.classList.add('is-selected');
             day.setAttribute('aria-selected', 'true');
-          });
+          }
+          if (!cursorActive) {
+            console.log('!cursorActive');
+          }
         });
       });
+
+
+
     }
   });
 
 
-  
+
 });
